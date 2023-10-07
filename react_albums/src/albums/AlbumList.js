fbf264c53d6c2ed1a8bfe19e01c09e32004cd6da
jwgilliam <ggsmoustache@gmail.com>
2023-10-07 18:35:04 -0500

    func: built provider

---
+++ react_albums/src/albums/AlbumList.js
@@ -0,0 +1,4 @@
+import "AlbumList.css"
+import Album from "./Album"
+import { albumContext } from "./AlbumProvider"
+import { useContext } from "react"
--- react_albums/src/albums/AlbumProvider.js
+++ react_albums/src/albums/AlbumProvider.js
@@ -1,7 +1,33 @@
-import { useContext, createContext, useEffect } from "react";
+import { useContext, createContext, useEffect, useState } from "react";
 
 export const albumContext = createContext()
 
 export function AlbumProvider(props) {
+  const [albums, setAlbums] = useState([])
 
+  useEffect(() => {
+    getAlbums()
+    console.log("check")
+  }, [])
+
+  const getAlbums = () => {
+    return fetch("http://localhost:8088/albums")
+      .then((response) => response.json())
+      .then((response) => setAlbums(response))
+  }
+
+  const addAlbum = (album) => {
+    return fetch("http://localhost:8088/albums", {
+      method: "Post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(album)
+    })
+      .then(getAlbums)
+  }
+
+  return (
+    <albumContext.Provider value={{ albums, addAlbum }}>{props.children}</albumContext.Provider>
+  )
 }
