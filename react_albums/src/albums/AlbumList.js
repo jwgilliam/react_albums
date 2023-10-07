8d063fbd347d7e91a5e0330ed79018c4fa097f00
jwgilliam <ggsmoustache@gmail.com>
2023-10-07 18:42:27 -0500

    func: built list

--- react_albums/src/albums/AlbumList.js
+++ react_albums/src/albums/AlbumList.js
@@ -2,3 +2,14 @@ import "AlbumList.css"
 import Album from "./Album"
 import { albumContext } from "./AlbumProvider"
 import { useContext } from "react"
+
+export default function AlbumList() {
+  const { albums } = useContext(albumContext)
+  return (
+    <div className="album_list">
+      {albums.map((album) => {
+        return <Album album={album.title} key={album.id} />
+      })}
+    </div>
+  )
+}
