edf17ddf0aeaa2b48cfd0539ad6ff8cc77efde9e
jwgilliam <ggsmoustache@gmail.com>
2023-10-12 18:19:41 -0500

    func: working on cleaning up edit functionality

--- react_albums/src/albums/Album.js
+++ react_albums/src/albums/Album.js
@@ -2,22 +2,23 @@ import "./Album.css"
 import { albumContext } from "./AlbumProvider"
 import { useContext } from "react"
 
-export default function Album({ album }) {
+export default function Album(props) {
   const { deleteAlbum, setSelectedAlbum } = useContext(albumContext)
 
   const deleteHandler = () => {
-    deleteAlbum(album)
+    deleteAlbum(props.album)
   }
 
   const editHandler = () => {
-    setSelectedAlbum(album)
+    setSelectedAlbum(props.album)
+    console.log(props.album)
   }
 
   return (
     <div className="album_card">
-      <p>{album.albumTitle}</p>
-      <p>{album.albumArtist}</p>
-      <p>{album.albumRating}</p>
+      <p>{props.album.albumTitle}</p>
+      <p>{props.album.albumArtist}</p>
+      <p>{props.album.albumRating}</p>
       <button className="delete_album" onClick={deleteHandler}>Delete</button>
       <button className="edit_album" onClick={editHandler}>Edit</button>
 
